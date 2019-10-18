070918ce5812f076fa625dc062b9cd965205c40c
Gustavo Henrique Pinto <ghp2201@gmail.com>
2019-10-18 16:36:34 -0300

    inicio do index

---
+++ funcoes.php
@@ -0,0 +1,29 @@
+<?php
+    function postagem($fotoUsuario, $nomeUsuario, $textoPostagem, $fotosPostagem)
+    {
+        echo "
+            <div class = \"postagem\">
+                <div class = \"fotoUsuario\">
+                    $fotoUsuario
+                </div>
+                <div class = \"nomeUsuario\">
+                    $nomeUsuario
+                </div>
+                <div class = \"fonteCorpo\">
+                    $textoPostagem
+                </div>
+                <div class = \"fotoCorpo\">
+                    $fotosPostagem
+                </div>
+                <div class = \"botoesCorpo\">
+                    <table>
+                        <tr>
+                            <td><a href = \"curtir.php\" class = \"fonteBotoesCorpo\"c><img src = \"icones/gostei2.png\" class = \"iconeBotoesCorpo\">Gostei</a></td>
+                            <td><a href=\"compartilhar.php\" class = \"fonteBotoesCorpo\"><img src = \"icones/compartilhar2.png\" class = \"iconeBotoesCorpo\">Compartilhar</a></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        ";
+    }
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "UTF-8">
+        <meta http-equiv = "X-UA-Compatible" content = "IE = edge"/>
+        <meta name = "viewport" content = "width = device-width, initial-scale = 1"/>
+        <link href = "bootstrap/css/bootstrap.min.css" rel = "stylesheet " media = "screen">
+        <script src = "js/jquery.min.js"></script>
+		<script src = "bootstrap/js/bootstrap.min.js"></script>
+		<link type = "text/css" rel = "stylesheet" href = "estilo.css">
+    </head>
+    <body>
+        <div class = "menuPerfil">
+            <a href = "meuPerfil.php"><img src = "icones/perfil2.png"></a>
+        </div>
+        <div class = "menu">
+            <a href = "index.php" class = "botoesMenu"><img src = "icones/home.png"/></a></td>
+            <a href s= "pesquisar.php" class = "botoesMenu"><img src = "icones/pesquisar2.png"/></a>
+            <a href = "mensagens.php" class = "botoesMenu"><img src = "icones/mensagem2.png"/></a>
+            <a href = "mural.php" class = "botoesMenu"><img src = "icones/mural2.png"></a>
+        </div>
+        <div class = "corpo">
+            <?php
+                require "funcoes.php";
+                $fotoUsuario = "<img src = \"icones/perfil.png\">";
+                $nomeUsuario = "Administrador";
+                $textoPostagem = "Texto de exemplo 777 rafa moreira mano";
+                $fotosPostagem = "";
+                postagem($fotoUsuario, $nomeUsuario, $textoPostagem, $fotosPostagem);
+            ?>
+        </div>
+    </body>
+</html>
