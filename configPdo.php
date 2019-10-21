fdf00fd9897cc1542033ecea63256545fe3d57cb
gustavo hnerique pinto <ghp2201@gmail.com>
2019-10-21 19:27:25 -0300

    inicio da conexão com o bd, iframe removido

--- configPdo.php
+++ configPdo.php
@@ -2,5 +2,6 @@
     //conexão com o banco de dados orbis.sql
     $pdo = new PDO('mysql:host=localhost;dbname=orbis', 'root', '', array(PDO::ATTR_PERSISTENT => true));
     $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //definine codificação padrão UTF 8
     $pdo -> exec("SET CHARACTER SET utf8");
 ?>
